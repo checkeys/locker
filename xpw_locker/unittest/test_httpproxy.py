db8a2e03fdf42a0cf189f9a6aa28f9cf104e1523
Mingzhe Zou <zoumingzhe@outlook.com>
2025-04-13 14:47:28 +0800

    httpproxy: move command-line to httpproxy module

--- xpw_locker/unittest/test_httpproxy.py
+++ xpw_locker/unittest/test_httpproxy.py
@@ -1,7 +1,10 @@
 # coding:utf-8
 
+from errno import ECANCELED
 import unittest
 from unittest import mock
+
+from xpw.authorize import Argon2Auth
 
 from xpw_locker import httpproxy
 
@@ -27,6 +30,29 @@ class TestServer(unittest.TestCase):
         listen_address = ("0.0.0.0", 8080)
         target_url = "https://example.com/"
         self.assertIsNone(httpproxy.run(listen_address, target_url))
+
+
+class TestCommand(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @mock.patch.object(httpproxy, "run")
+    @mock.patch.object(httpproxy.AuthInit, "from_file")
+    def test_main(self, mock_auth, _):
+        mock_auth.side_effect = [Argon2Auth({"users": {"test", "unit"}})]
+        self.assertEqual(httpproxy.main([]), ECANCELED)
 
 
 if __name__ == "__main__":
