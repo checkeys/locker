a0d4befed8b54cc31c32bec8facb4cf8b2df35fd
Mingzhe Zou <zoumingzhe@qq.com>
2025-04-17 19:56:36 +0800

    bump version to 0.3.1

--- xpw_locker/attribute.py
+++ xpw_locker/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xpw-locker"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __urlhome__ = "https://github.com/podboy/locker/"
 __description__ = "Access authentication"
 __official_name__ = "Locker"
