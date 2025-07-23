b598c048dcbb999966006e644ea4bd628d5c2deb
Mingzhe Zou <zoumingzhe@qq.com>
2025-07-23 18:41:00 +0800

    bump version to 0.7.2

--- xpw_locker/attribute.py
+++ xpw_locker/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xpw-locker"
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 __urlhome__ = "https://github.com/checkeys/locker/"
 __description__ = "Access authentication"
 __official_name__ = "Locker"
